78f01d4a198172046c2d8d83d93f7be698c77317
Matheus da Cunha <matheus.cunha.fonseca50@gmail.com>
2023-02-02 16:03:31 -0300

    Adicionei o modal

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import initScroll from "./modules/scroll-anime.js";
 import initSmoothScroll from "./modules/scroll-smooth.js";
 import initAccordionList from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js";
+import initModal from "./modules/modal.js";
 
 initTabNav();
 initAccordionList();
 initSmoothScroll();
 initScroll();
+initModal();
