4b9bbbfd9f6403091e798ca5e46603e4786922ab
Matheus da Cunha <matheus.cunha.fonseca50@gmail.com>
2023-02-02 16:52:35 -0300

    Adicionei tooltip ao mapa

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import initSmoothScroll from "./modules/scroll-smooth.js";
 import initAccordionList from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
+import initToolTip from "./modules/tooltip.js";
 
 initTabNav();
 initAccordionList();
 initSmoothScroll();
 initScroll();
 initModal();
+initToolTip();
